2149beee508dc2f5579e83df209b18eda97f021d
PHILIP OBIORAH <philip.obiorah@outlook.com>
2025-05-03 23:17:49 +0100

    Update the /notes route to save the note

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from app.models import NoteCreate
+from fastapi import Depends
+from sqlalchemy.orm import Session
+from app.database import SessionLocal, Note, get_db
+
 
 app = FastAPI()
 
@@ -10,9 +14,17 @@ def root():
     return {"message": "Welcome to the AI Notes App!"}
 
 
+# @app.post("/notes")
+# def create_note(note: NoteCreate):
+#     return {
+#         "title": note.title,
+#         "content": note.content
+#     }
+
 @app.post("/notes")
-def create_note(note: NoteCreate):
-    return {
-        "title": note.title,
-        "content": note.content
-    }
+def create_note(note: NoteCreate, db: Session = Depends(get_db)):
+    new_note = Note(title=note.title, content=note.content)
+    db.add(new_note)
+    db.commit()
+    db.refresh(new_note)
+    return {"id": new_note.id, "title": new_note.title, "content": new_note.content}
--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, Field
 
 class NoteCreate(BaseModel):
-    title: str = Field(..., min_length=1, max_length=100, example="My Note Title")
-    content: str = Field(..., min_length=1, example="This is the content of my note.")
+    title: str = Field(..., min_length=1)
+    content: str = Field(..., min_length=1)
