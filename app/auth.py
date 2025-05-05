833911fd3e08d6cc530de34136b5e71efbb34e27
PHILIP OBIORAH <philip.obiorah@outlook.com>
2025-05-05 22:22:08 +0100

    Added the parent directory to the path (fix for macOS)

--- app/auth.py
+++ app/auth.py
@@ -1,10 +1,7 @@
 from fastapi import Header, HTTPException
 
+API_KEY = "mysecretkey"
 
-# API key security logic
-API_KEY = "MySecretAPIKey"
-
-def verify_api_key(x_api_key: str = Header(...)):
+def verify_api_key(x_api_key: str = Header(..., alias="x-api-key")):
     if x_api_key != API_KEY:
-        raise HTTPException(status_code=403, detail="Forbidden: Invalid API Key")
-    
+        raise HTTPException(status_code=403, detail="Invalid API Key")
